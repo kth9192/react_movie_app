301544063ce646ce95db31466351fad419cc99a7
kth919 <kth9193@gmail.com>
2019-11-16 14:38:21 +0900

    rjx 와 props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,62 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({ favorite }) {
-  console.log(favorite);
-  return <h1>i like {favorite}</h1>;
+function Food({ name, picture, rating }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} alt={name} />
+      <h3>{rating}/5.0</h3>
+    </div>
+  );
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
+
+const foodILike = [
+  {
+    id: 1,
+    name: "kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 2
+  },
+  {
+    id: 2,
+    name: "kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating: 3
+  },
+  {
+    id: 3,
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+    rating: 5
+  }
+];
+
+function renderFood(dish) {
+  return (
+    <Food
+      key={dish.id}
+      name={dish.name}
+      picture={dish.image}
+      rating={dish.rating}
+    />
+  );
 }
 
 function App() {
   return (
     <div className="App">
-      <h1>hello, world</h1>
-      <Food favorite="kimchi" contry="america" />
+      {console.log(foodILike.map(renderFood))}
+      {foodILike.map(renderFood)}
     </div>
   );
 }
